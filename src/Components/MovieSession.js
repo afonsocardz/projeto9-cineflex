b7b8e821f53674214640569d69d34583da55982a
Afonso Cardozo Cruz <afonso.desenvolvimento@gmail.com>
2022-05-23 12:28:52 -0300

    Fine touching to style

--- src/Components/MovieSession.js
+++ src/Components/MovieSession.js
@@ -1,16 +1,17 @@
 import { Link } from "react-router-dom";
 import ColumnContainer from "./Styles/ColumnContainer";
 import FlexContainer from "./Styles/FlexContainer";
+import styled from "styled-components";
 
 export default function MovieSession({ session, user, setUser }) {
     const { showtimes } = session;
     return (
         <ColumnContainer padSide={"25px"}>
-            <span>{`${session.weekday} - ${session.date}`} </span>
+            <Span>{`${session.weekday} - ${session.date}`} </Span>
             <FlexContainer>
                 {showtimes.map(showtime =>
                     <Link to={`/sessao/${showtime.id}`}>
-                        <button type="button" onClick={() => setUser(
+                        <Button type="button" onClick={() => setUser(
                             {
                                 ...user,
                                 session: {
@@ -20,10 +21,22 @@ export default function MovieSession({ session, user, setUser }) {
                                     time: showtime.name
                                 }
                             })
-                        }>{showtime.name}</button>
+                        }>{showtime.name}</Button>
                     </Link>
                 )}
             </FlexContainer>
         </ColumnContainer>
     );
 }
+
+const Button = styled.button`
+    width: 82px;
+    height: 43px;
+    margin: 23px 0;
+    margin-right: 8px;
+`;
+
+const Span = styled.span`
+    font-size: 20px;
+    padding-bottom: 1px;
+`;
